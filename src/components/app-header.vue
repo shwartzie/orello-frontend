c1ae15bd977796815832603896b999c3576cb2a1
Shaul Lavochkin <shaul919191@gmail.com>
2022-07-19 21:34:16 +0300

    added header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,52 +1,71 @@
 <template>
     <header>
         <nav>
-            <router-link to="/">Home</router-link>
-            <p></p>
-            <router-link to="/boards">boards</router-link>
 
-            <!-- <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
-                    @select="handleSelect">
-                    <el-menu-item index="0">LOGO</el-menu-item>
-                    <div class="flex-grow" />
-                    <el-menu-item index="1">Processing Center</el-menu-item>
-                    <el-sub-menu index="2">
-                        <template #title>Workspace</template>
-                        <el-menu-item index="2-1">item one</el-menu-item>
-                        <el-menu-item index="2-2">item two</el-menu-item>
-                        <el-menu-item index="2-3">item three</el-menu-item>
-                        <el-sub-menu index="2-4">
-                            <template #title>item four</template>
-                            <el-menu-item index="2-4-1">item one</el-menu-item>
-                            <el-menu-item index="2-4-2">item two</el-menu-item>
-                            <el-menu-item index="2-4-3">item three</el-menu-item>
-                        </el-sub-menu>
+
+            <el-menu class="el-menu-demo" mode="horizontal" @select="handleSelect">
+
+                <el-menu-item index="1">
+                    <router-link to="/">Trello</router-link>
+                </el-menu-item>
+                <el-menu-item index="2">
+                    <router-link to="/boards">Boards</router-link>
+                </el-menu-item>
+                <el-sub-menu>
+                    <template #title>Workspace</template>
+                    <el-menu-item index="3-1">item one</el-menu-item>
+                    <el-menu-item index="3-2">item two</el-menu-item>
+                    <el-menu-item index="3-3">item three</el-menu-item>
+                    <el-sub-menu index="3-4">
+                        <template #title>item four</template>
+                        <el-menu-item index="4-4-1">item one</el-menu-item>
+                        <el-menu-item index="4-4-2">item two</el-menu-item>
+                        <el-menu-item index="4-4-3">item three</el-menu-item>
                     </el-sub-menu>
-                </el-menu> -->
+                </el-sub-menu>
+                <el-menu-item index="5">
+                    <router-link to="/board">Board</router-link>
+                </el-menu-item>
+            </el-menu>
+            <div class="h-6" />
+
         </nav>
     </header>
 </template>
+
 <script>
-
-
 export default {
 
     data() {
         return {
-            activeIndex: 1
+
         }
     },
-    methods: {}
+    methods: {
+        handleSelect(key, path) {
+            console.log(key, path)
+        }
+    }
 
 
 }
 </script>
 
 
+ <style>
+ .flex-grow {
+     flex-grow: 1;
+ }
+ </style>
 
 
-            <style>
-            .flex-grow {
-                flex-grow: 1;
-            }
-            </style>
+
+<!-- <script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const activeIndex2 = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script> -->
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import store from './store/index'
 import './assets/styles/main.scss'
 const app = createApp(App)
