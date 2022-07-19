68a3e5404b6f7a7827fca7d2a45d4692481782a1
shwartzie <ronnybusiness77@gmail.com>
2022-07-19 16:42:10 +0300

    initialized file structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHashHistory } from "vue-router"
+import homePage from "../views/home-page"
+import aboutPage from "../views/about-page"
+import boardPreview from "../components/board-preview"
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: homePage,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: aboutPage,
+        },
+        {
+            path: "/board/:id",
+            name: "board-preview",
+            component: boardPreview,
+        },
+        // {
+        //   path: '/toy/:id',
+        //   name: 'toy-details',
+        //   component:toyDetails,
+        // },
+        // {
+        //   path: '/toy/edit/:id?',
+        //   name: 'toy-edit',
+        //   component: toyEdit,
+        // },
+    ],
 })
 
 export default router
