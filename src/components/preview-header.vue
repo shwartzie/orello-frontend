5e4b20e5d6645842f47f8e772f4aeb0b9bc920f9
shwartzie <ronnybusiness77@gmail.com>
2022-07-21 00:08:43 +0300

    fixed ux/ui in board-preview nav-bar, added css to task-modal

--- src/components/preview-header.vue
+++ src/components/preview-header.vue
@@ -7,13 +7,11 @@
                     <span>board</span>
                     <img src="../assets/svg/board-arrow-down.svg" />
                 </a>
-                <h1 class="board-header-btn board-header-title">{{
-                    board.title
-                }}</h1>
+                <h1 class="board-header-btn board-header-title">
+                    {{ board.title }}
+                </h1>
                 <div class="board-header-template-span" v-if="!user.loggedIn">
-                    <div>
-                        Template
-                    </div>
+                    <div>Template</div>
                 </div>
                 <a
                     class="board-header-btn star-btn"
@@ -25,33 +23,47 @@
             </div>
             <a class="board-header-btn">trello workspace</a>
             <span class="board-header-btn-divider"></span>
-            <a class="board-header-btn"
-                ><i class="fa-solid fa-user-group"></i>workspace visible</a
-            >
+            <a class="board-header-btn workspace-visble">
+                <span>
+                    <i class="fa-solid fa-user-group"></i>
+                </span>
+                workspace visible
+            </a>
             <span class="board-header-btn-divider"></span>
             <div class="board-header-btn">
                 <a v-for="member in board.members">
                     {{ member.fullname }}
                 </a>
             </div>
-            <a class="board-header-btn"
-                ><i class="fa-solid fa-user-plus"></i>share</a
+            <a class="board-header-btn board-share-button"
+                >
+                <span>
+                    <i class="fa-solid fa-user-plus"></i>
+                </span>
+                share</a
             >
         </section>
         <section class="flex">
             <div class="flex">
                 <a class="board-header-btn">slack</a>
-                <a class="board-header-btn"
-                    ><i class="fa-solid fa-rocket"></i>power ups</a
-                >
-                <a class="board-header-btn"
-                    ><i class="fa-solid fa-bolt"></i>automation</a
-                >
+                <a class="board-header-btn power-up-btn">
+                    <span> <i class="fa-solid fa-rocket"></i> </span>
+                    power ups
+                </a>
+                <a class="board-header-btn automation-btn">
+                    <span>
+                        <i class="fa-solid fa-bolt"></i>
+                    </span>
+                    automation
+                </a>
             </div>
             <div class="flex">
                 <span class="board-header-btn-divider"></span>
-                <a class="board-header-btn"
-                    ><i class="fa-solid fa-filter"></i>filter</a
+                <a class="board-header-btn filter-btn">
+                    <span>
+                        <i class="fa-solid fa-filter"></i>
+                    </span>
+                    filter</a
                 >
                 <a class="board-header-btn board-header-show-menu"
                     ><i class="fa-solid fa-ellipsis"></i>show menu</a
@@ -67,7 +79,7 @@ export default {
     data() {
         return {
             isStarred: false,
-            user: {loggedIn: false}
+            user: { loggedIn: false },
         }
     },
     props: {
@@ -81,7 +93,9 @@ export default {
     },
     computed: {
         getStar() {
-            return this.isStarred ? "fa-regular fa-star" : "fa-solid fa-star fa-star-colored"
+            return this.isStarred
+                ? "fa-regular fa-star"
+                : "fa-solid fa-star fa-star-colored"
         },
     },
     mounted() {},
--- src/components/task-modal.vue
+++ src/components/task-modal.vue
@@ -1,22 +1,26 @@
 <template>
     <section class="task-modal">
         <section class="task-modal-info">
-            <img v-if="task.attachments" src=task.attachments[0] alt="">
+            <img v-if="task.attachments" src="task.attachments[0]" alt="" />
             <header class="window-header">
                 <div class="flex space-between">
-                    <div class="flex ">
+                    <div class="flex">
                         <i class="fa-solid fa-hard-drive"></i>
                         <div class="flex column title-modal">
-                            {{ task.title }}
-                            <h6> in list {{ group.title }}</h6>
+                            Android App new landing page
+                            <p>
+                                in list
+                                <a>
+                                    {{ group.title }}
+                                </a>
+                            </p>
                         </div>
                     </div>
-                    <a @click="closeModal">X</a>
+                    <a class="task-close-modal" @click="closeModal"><i class="fa-solid fa-x"></i></a>
                 </div>
             </header>
             <!-- modal body -->
             <section class="flex space-between">
-
                 <section>
                     <div class="flex labels" v-if="group.labels">
                         <h5>Labels</h5>
@@ -25,11 +29,14 @@
                         </div>
                         <a>+</a>
                     </div>
-                    <div class="flex ">
+                    <div class="flex">
                         <i class="fa-solid fa-align-justify"></i>
                         <div class="flex column description">
                             Description
-                            <textarea v-if="task.description" contenteditable="true"></textarea>
+                            <textarea
+                                v-if="task.description"
+                                contenteditable="true"
+                            ></textarea>
                             <a href=""> add a detailed description</a>
                         </div>
                     </div>
@@ -38,7 +45,7 @@
                         <div class="flex column attachments">
                             attachments
                             <div v-for="attachment in task.attachments">
-                                <img src=attachment.src alt="">
+                                <img src="attachment.src" alt="" />
                             </div>
                             <a href=""> add a an attachment</a>
                         </div>
@@ -50,43 +57,138 @@
                             <div v-for="activity in task.activities">
                                 <p>activity</p>
                             </div>
-                            <input type="text" placeholder="write a comment">
+                            <input type="text" placeholder="write a comment" />
                         </div>
                     </div>
                 </section>
                 <!-- side bar -->
                 <section class="flex column">
                     <div class="flex column side-bar">
-                        <h4>add to card</h4>
-                        <a class="board-header-btn side-bar-button" href=""><i class="fa-solid fa-user"></i> members</a>
-                        <a class="board-header-btn side-bar-button" href=""><i class="fa-solid fa-tag"></i>Labels</a>
-                        <a class="board-header-btn side-bar-button" href=""><i
-                                class="fa-solid fa-square-check"></i>Checklist</a>
-                        <a class="board-header-btn side-bar-button" href=""><i class="fa-solid fa-clock"></i>Dates</a>
-                        <a class="board-header-btn side-bar-button" href=""><i
-                                class="fa-solid fa-paperclip"></i>Attachment</a>
-                        <a class="board-header-btn side-bar-button" href="">custom Fields</a>
+                        <h4>Add to card</h4>
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-user"></i>
+                            </span>
+                            members</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-tag"></i>
+                            </span>
+                            Labels</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-square-check"></i>
+                            </span>
+                            Checklist</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-clock"></i>
+                            </span>
+                            Dates</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-paperclip"></i>
+                            </span>
+                            Attachment</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                            >custom Fields</a
+                        >
                     </div>
                     <div class="flex column">
                         <h4>power ups</h4>
-                        <a class="board-header-btn side-bar-button" href="">Confluence</a>
-                        <a class="board-header-btn side-bar-button" href="">+ add new power</a>
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                            >Confluence</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                            >+ add new power</a
+                        >
                     </div>
                     <div class="flex column">
                         <h4>automation</h4>
-                        <a class="board-header-btn side-bar-button" href="">+ add button</a>
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                            >+ add button</a
+                        >
                     </div>
                     <div class="flex column">
                         <h4>axtions</h4>
-                        <a class="board-header-btn side-bar-button" href=""><i
-                                class="fa-solid fa-arrow-right"></i>move</a>
-                        <a class="board-header-btn side-bar-button" href=""><i class="fa-solid fa-copy"></i>copy</a>
-                        <a class="board-header-btn side-bar-button" href="">make template</a>
-                        <a class="board-header-btn side-bar-button" href=""><i class="fa-solid fa-eye"></i>watch</a>
-                        <a class="board-header-btn side-bar-button" href=""><i
-                                class="fa-solid fa-box-archive"></i>archive</a>
-                        <a class="board-header-btn side-bar-button" href=""><i
-                                class="fa-solid fa-square-share-nodes"></i>share</a>
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-arrow-right"></i>
+                            </span>
+                            move</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-copy"></i>
+                            </span>
+                            copy</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                            >make template</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-eye"></i>
+                            </span>
+                            watch</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-box-archive"></i>
+                            </span>
+                            archive</a
+                        >
+                        <a
+                            class="board-header-btn button-link side-bar-button"
+                            href=""
+                        >
+                            <span>
+                                <i class="fa-solid fa-square-share-nodes"></i>
+                            </span>
+                            share</a
+                        >
                     </div>
                 </section>
             </section>
@@ -94,8 +196,7 @@
     </section>
 </template>
 
-<script >
-
+<script>
 export default {
     props: {
         task: Object,
@@ -104,15 +205,15 @@ export default {
     data() {
         return {}
     },
-    created() { },
+    created() {},
     methods: {
         closeModal() {
             this.$emit("closeModal")
-        }
+        },
     },
     computed: {},
-    mounted() { },
-    unmounted() { },
-    components: {}
+    mounted() {},
+    unmounted() {},
+    components: {},
 }
 </script>
