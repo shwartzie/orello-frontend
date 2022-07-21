7a5a027f86540c467b848a3a0933463bc5e0e7f0
shwartzie <ronnybusiness77@gmail.com>
2022-07-21 16:57:24 +0300

    syncing up

---
+++ src/components/group-cmps/group-actions.vue
@@ -0,0 +1,31 @@
+<template>
+    <a @click="displayModal = !diplayModal" :class="popOver" class="board-header-btn board-header-show-menu">
+        <i
+            class="fa-solid fa-ellipsis"
+            style="color: #172b4d; opacity: 0.4; font-size: 13px"
+        ></i>
+    </a>
+</template>
+
+<script>
+export default {
+    props: {},
+    data() {
+        return {
+            displayModal: false
+        }
+    },
+    created() {},
+    methods: {
+        onWorkspaceVisible() {},
+    },
+    computed: {
+        popOver() {
+            // return this.displayModal ? 'pop-over' : ''
+        }
+    },
+    mounted() {},
+    unmounted() {},
+    components: {},
+}
+</script>
