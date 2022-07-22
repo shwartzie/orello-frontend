7d31ef48c713db102d23ec57cea27b3a11dfb055
Nehoray Genis <bvurth000100@gmail.com>
2022-07-22 23:04:05 +0300

    deleted modal

--- src/components/costume-card-cmp.vue
+++ src/components/costume-card-cmp.vue
@@ -1,42 +1,6 @@
 <template>
-    <div class="members-btn">
-        <a
-            @click="onDisplayModal"
-            class="board-header-btn button-link side-bar-button"
-        >
-            <span>
-                <i class="fa-solid fa-user"></i>
-            </span>
-            members
-        </a>
-        <div class="pop-over" :class="isShown">
-            <div class="pop-over-header">
-                <span class="pop-over-header-title">Members</span>
-                <a class="pop-over-header-close-btn">
-                    <i class="fa-solid fa-x" style="cursor:pointer;" @click="onDisplayModal"></i>
-                </a>
-            </div>
-            <div>
-                <div class="pop-over-content">
-                    <el-input v-model="filterBy.name" placeholder="Search members" />
-                    <div class="pop-over-section">
-                        <h4>Board members</h4>
-                    </div>
-                    <ul class="pop-over-member-list">
-                        <li v-for="member in members" :key="member._id">
-                            <a @click="onMember(member)">
-                                <span>
-                                    <img class="member-avatar" :src="member.imgUrl" />
-                                </span>
-                                <span>
-                                    {{ member.name }}
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+    <div class="pop-over-content">
+        <h1>hi</h1>
     </div>
 </template>
 
