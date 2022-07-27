8a4ea0bcdd7e77dc80fb818f238bc33e2091e59b
Nehoray Genis <bvurth000100@gmail.com>
2022-07-28 01:52:20 +0300

    fixed dup

--- src/components/task-cmps/task-modal-preview.vue
+++ src/components/task-cmps/task-modal-preview.vue
@@ -60,7 +60,6 @@
             <div class="flex activities window-module column">
                 <div class="flex space-between">
                     <div class="flex activity-title">
-
                         <span class="title-icon activity"></span>
                         <span class="task-modal-title-container title-sub">Activity</span>
                     </div>
@@ -75,14 +74,22 @@
                 <div class="flex column">
 
                     <div class="task-modal-layout flex">
-                        <img class="member-avatar" :src="loggedinUser.imgUrl" />
-                        <div class="comment-box flex">
+                        <img class="member-avatar" :src="loggedinUser.imgUrl" v-if="task.activities?.length" />
+                        <div class="comment-box flex column" @click="addComment = !addComment">
                             <input class="comment-box-input js-new-comment-input" v-if="!board.isStatic" type="text"
-                                placeholder="write a comment" />
+                                placeholder="write a comment" v-model="newComment" />
+                            <div v-if="addComment">
+                                <a class="button-primary" disabled @click="saveComment">save</a>
+                            </div>
+
                         </div>
                     </div>
-                    <div v-for="activity in task.activities" class="flex">
-                        <img class="member-avatar" :src="activity.byUser.imgUrl" />
+                    <div v-for="activity in task.activities" class="flex column" v-if="task.activities?.length">
+                        <div>
+                            <img class="member-avatar" :src="activity.byUser.imgUrl" />
+                            <span>{{activity.byUser.fullname}}</span>
+                            <span>{{activity.createdAt}}</span>
+                        </div>
                         <p>{{ activity.txt }}</p>
                     </div>
                 </div>
@@ -191,11 +198,31 @@ export default {
             displayCover: false,
             toDisplayLabelModal: false,
             sideLabelModal: false,
+            addComment: false,
+            newComment: "",
         }
     },
     methods: {
-        showUserProfile() {
+        saveComment() {
+            const currBoard = JSON.parse(JSON.stringify(this.board))
+            const currGroup = JSON.parse(JSON.stringify(this.group))
+            const taskToAdd = JSON.parse(JSON.stringify(this.task))
+            const { tasks } = currGroup
+            const tasksIdx = tasks.findIndex((task) => task.id === taskToAdd.id)
+            const user = userService.getLoggedinUser()
+            taskToAdd.activities.unshift({
+                byUser: user,
+                txt: `${this.newComment} `,
+                createdAt: Date.now(),
+            })
+            currGroup.tasks[tasksIdx] = taskToAdd
 
+            this.$store.dispatch({
+                type: "updateTask",
+                currBoard,
+                currGroup,
+                taskToAdd,
+            })
         },
         onCloseCoverModal() {
             this.displayCover = false
