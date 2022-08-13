0a8d62491ff0cb873d272539bb222c118348c0f3
shwartzie <ronnybusiness77@gmail.com>
2022-08-13 13:14:53 +0300

    added filtering boards by title, added router push for the selectedboard (route gets id but doesnt render on click the selected board, needs to be fixed, and remove the selected board from search bar)

--- src/components/header-cmps/search-bar.vue
+++ src/components/header-cmps/search-bar.vue
@@ -1,7 +1,8 @@
 <template>
-    <el-select v-if="filterBy.boards" class="search-bar-main-nav" v-model="filterBy.boardTitle" @input="remoteMethod" multiple filterable
-        remote reserve-keyword placeholder="Search..." :remote-method="remoteMethod" :loading="loading">
-        <el-option  v-for="board in filterBy.boards" :key="board._id" :value="board.title">
+    <el-select v-if="filterBy.boards" class="search-bar-main-nav" v-model="filterBy.boardTitle" @input="remoteMethod"
+        multiple filterable remote reserve-keyword placeholder="Search..." :remote-method="remoteMethod"
+        :loading="loading">
+        <el-option v-for="board in filterBy.boards" :key="board._id"  :value="board.title" @click="onBoard(board._id)">
         </el-option>
         <template #prefix>
             <el-icon class="el-input__icon">
@@ -28,21 +29,26 @@ export default {
             loading: false,
         }
     },
-    created() {
+    mounted() {
         this.filterBy.boards = this.boards.map(board => board)
     },
     methods: {
-       
+        onBoard(boardId) {
+            console.log('this.$router:',this.$router);
+            this.$router.push(`/board/${boardId}`)
+        },
         remoteMethod(query) {
             if (query !== '') {
                 this.loading = true
-                this.filterBy.boards = this.boards.filter(board => {
+                return this.filterBy.boards = this.boards.filter(board => {
                     this.loading = false
                     const regex = new RegExp(query, "i")
                     return regex.test(board.title)
                 })
+                
             }
             this.filterBy.boards = []
+           
         }
     },
     components: {
